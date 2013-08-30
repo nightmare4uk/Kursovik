429a4a9ff694a697a40f9004fafa96793386d13b
nightmare4uk <jokobako@gmail.com>
2013-08-30 20:50:45 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kursovoi::Application.config.secret_key_base = '5fa45293e81b4408070e8885204fb4b4b86148fe5be8aabb2180a8234883d5b29b314862bbeee33a6559fc8cad3ff5057466d49705818486eae38ee6f4a10516'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kursovoi::Application.config.session_store :cookie_store, key: '_Kursovoi_session'
